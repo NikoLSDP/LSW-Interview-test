d6df19cef590a39de9c13f41f76e45edc7f1502d
NicoPizia <81713615+NicoPizia@users.noreply.github.com>
2022-07-16 20:49:02 -0300

    first commit

---
+++ Assets/_Game/_Dev/Scripts/StateManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { Game, Shop, Inventory }
+
+public class StateManager : MonoBehaviour
+{
+    public static StateManager instance = null;
+    public GameState state;
+
+    #region EVENTS
+    public delegate void OnGameStateChanged(GameState _state);
+    public static OnGameStateChanged onGameStateChanged;
+
+    #endregion
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void ChangeGameState(GameState _newState)
+    {
+        state = _newState;
+        onGameStateChanged?.Invoke(_newState);
+    }
+
+
+}
